31ca22ba492dc55eea519ba3bd266069bbb92964
Troy Rysedorph <troy.rysedorph@capitalone.com>
2015-08-05 14:09:13 -0400

    Put wemo find and exercise code into project also POM updates

--- my-app/src/main/java/com/hackathon/app/App.java
+++ my-app/src/main/java/com/hackathon/app/App.java
@@ -1,5 +1,8 @@
 package com.hackathon.app;
 
+import com.palominolabs.wemo.InsightSwitch;
+import com.palominolabs.wemo.InsightSwitchFinder;
+
 /**
  * Hello world!
  *
@@ -9,5 +12,26 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        
+        String switchFriendlyName = "OneMo";
+        try (InsightSwitchFinder insightSwitchFinder = new InsightSwitchFinder(switchFriendlyName)) {
+            if (!insightSwitchFinder.findSwitches()) {
+                throw new IllegalStateException("Unable to find switches");
+            }
+
+            InsightSwitch insightSwitch = insightSwitchFinder.getSwitch(switchFriendlyName);
+
+            while (true) {
+                insightSwitch.switchOn();
+                Thread.sleep(100);
+                insightSwitch.switchOff();
+                Thread.sleep(100);
+            }
+        
+        } catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        
     }
 }
